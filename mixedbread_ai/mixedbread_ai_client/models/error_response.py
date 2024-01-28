d11053f451ba5dfb5b0540914c35c55b96c6ebc0
Julius Lipp <lipp.julius@gmail.com>
2024-01-28 11:02:59 +0100

    bug: fix error codes

--- mixedbread_ai/mixedbread_ai_client/models/error_response.py
+++ mixedbread_ai/mixedbread_ai_client/models/error_response.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING, Any, Dict, List, Type, TypeVar
 
-import attr
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
 
 if TYPE_CHECKING:
     from ..models.error_response_data import ErrorResponseData
@@ -9,7 +10,7 @@ if TYPE_CHECKING:
 T = TypeVar("T", bound="ErrorResponse")
 
 
-@attr.s(auto_attribs=True)
+@_attrs_define
 class ErrorResponse:
     """
     Attributes:
@@ -21,11 +22,13 @@ class ErrorResponse:
     code: int
     message: str
     data: "ErrorResponseData"
-    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
         code = self.code
+
         message = self.message
+
         data = self.data.to_dict()
 
         field_dict: Dict[str, Any] = {}
